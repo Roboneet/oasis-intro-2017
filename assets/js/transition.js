08a375fcd3106572ce81fed461eb761ef8ea78b5
Nilay Arora <nilayarora1298@gmail.com>
2017-06-29 01:43:45 +0530

    Second try at tranisition

--- assets/js/transition.js
+++ assets/js/transition.js
@@ -1,18 +1,14 @@
 $('.slider .bar').click(function(){
 	var n = $('.slider .bar').index(this) + 1;
 	console.log(n);
-	$(".popup-container").css("display","block");
-	$(".popup-container .popup:nth-child(" + n + ")").css("display","flex");
-	$(".popup-container .popup:nth-child(" + n + ")").addClass("open");
+	$("#" + n).css("display","flex");
+	$("#" + n).addClass("open");
 	$(".popup #cross").click(function(){
-		console.log("I am clicked!");
-		$(".popup-container .popup:nth-child(" + n + ")").removeClass("open");
-		
+		$("#" + n).removeClass("open");
 		setTimeout(function(){
-			$(".popup-container").css("display","none");
-			$(".popup-container .popup:nth-child(" + n + ")").css("display","none");
-			$(".popup-container .popup:nth-child(" + n + ")").removeClass("close");
+			$("#" + n).css("display","none");
+			$("#" + n).removeClass("close");
 		},1100);
-		$(".popup-container .popup:nth-child(" + n + ")").addClass("close");
+		$("#" + n).addClass("close");
 	});
 })
