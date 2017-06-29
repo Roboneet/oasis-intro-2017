8554f3b168fdb2c3fd25d2ce2d73ee2a9e2606a7
Nilay Arora <nilayarora1298@gmail.com>
2017-06-29 22:29:24 +0530

    Alternate transition

--- assets/js/transition.js
+++ assets/js/transition.js
@@ -2,7 +2,9 @@ $('.slider .bar').click(function(){
 	var n = $('.slider .bar').index(this) + 1;
 	console.log(n);
 	$("#" + n).css("display","flex");
+	// $(".bar").addClass("closebar");
 	$("#" + n).addClass("open");
+
 	$(".popup #cross").click(function(){
 		$("#" + n).removeClass("open");
 		setTimeout(function(){
